dd8d3368f8d235da565eb2968399ef7d7728e007
Gustavo12386 <gucalderaro@hotmail.com>
2025-05-22 14:42:14 -0300

    Move games on list

--- src/main/java/com/intensivaojavaspring/dslist/services/GameListService.java
+++ src/main/java/com/intensivaojavaspring/dslist/services/GameListService.java
@@ -4,12 +4,9 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
-import com.intensivaojavaspring.dslist.dto.GameDTO;
 import com.intensivaojavaspring.dslist.dto.GameListDTO;
-import com.intensivaojavaspring.dslist.dto.GameMinDTO;
-import com.intensivaojavaspring.dslist.entities.Game;
 import com.intensivaojavaspring.dslist.entities.GameList;
+import com.intensivaojavaspring.dslist.projections.GameMinProjection;
 import com.intensivaojavaspring.dslist.repositories.GameListRepository;
 import com.intensivaojavaspring.dslist.repositories.GameRepository;
 
@@ -19,10 +16,29 @@ public class GameListService {
 	@Autowired
 	private GameListRepository gameListRepository;
 	
+	@Autowired
+	private GameRepository gameRepository;
+	
 	@Transactional(readOnly = true)
 	public List<GameListDTO> findAll(){
 		List<GameList> result = gameListRepository.findAll();
 		return result.stream().map(x -> new GameListDTO(x)).toList();
 		
+	}
+	
+	@Transactional
+	public void move(Long listId, int sourceIndex, int destinationIndex) {
+		List<GameMinProjection> list = gameRepository.searchByList(listId);
+		
+		// nesse ponto irá ocorrer a remoção do objeto em uma posição e ira adicionar em outra
+		GameMinProjection obj = list.remove(destinationIndex);
+		list.add(destinationIndex, obj);
+		
+		int min = sourceIndex < destinationIndex ? sourceIndex : destinationIndex;
+		int max = sourceIndex < destinationIndex ? destinationIndex : sourceIndex;
+		
+		for(int i = min; i <= max; i++) {
+			gameListRepository.updateBelongingPosition(listId, list.get(i).getId(), i);
+		}
 	}
 }
